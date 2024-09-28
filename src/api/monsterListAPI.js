3c6b772a98fa7faa32f79aa888a146983ebd0026
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-09-27 23:39:23 -0700

    Update monsterListAPI.js
    - created new fetch endpoint from FastAPI for individual monster detail info

--- src/api/monsterListAPI.js
+++ src/api/monsterListAPI.js
@@ -4,3 +4,8 @@ export const fetchMonsters = async () => {
   const response = await apiClient.get('/dqm1/monsters');
   return response.data;
 };
+
+export const fetchMonster = async (monster_id) => {
+  const response = await apiClient.get(`/dqm1/monstersandskill/${monster_id}`);
+  return response.data
+}
