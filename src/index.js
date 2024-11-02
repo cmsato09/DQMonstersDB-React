def0d286bb3e4e62f725f8d8e378738fe30a9f3b
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-11-01 19:03:05 -0700

    Update index.js with radix-ui/themes
    - import radix-ui/themes/styles.css to root of application
    - Add Theme component and wrapped around body of app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Theme>
+        <App />
+    </Theme>
 );
