f0a4a900b9bdcf33a9413cc60395e8faf4bf62dd
Chris Sato <cmsato09.git@gmail.com>
2025-05-05 14:24:23 -0700

    Add nav links and Back to Top link to Footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,17 +5,21 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4">
       <Container size="3">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-
-          {/* Navigation Links */}
-          <div className="flex space-x-4">
-            <a href="/about" className="hover:underline">About</a>
-            <a href="/contact" className="hover:underline">Contact</a>
-            <a href="/privacy" className="hover:underline">Privacy Policy</a>
+        <div className="flex flex-col md:flex-row justify-between items-center md:items-start">
+          {/* Back to Top Link */}
+          <div className="order-1 md:order-2 mb-4 md:mb-0 text-center md:text-right">
+            <a href="#top" className="text-gray-400 hover:text-white">
+              Back to Top
+            </a>
           </div>
 
-          <div className="flex space-x-4 mt-4 md:mt-0">
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:underline">GitHub Source</a>
+          {/* Navigation Links */}
+          <div className="order-2 md:order-1 flex flex-col items-center md:items-start">
+            <div className="flex space-x-4 mb-4 md:mb-0">
+              <a href="/about" className="hover:underline">About</a>
+              <a href="/contact" className="hover:underline">Contact</a>
+              <a href="https://github.com/cmsato09/DQMonstersDB-React" target="_blank" rel="noopener noreferrer" className="hover:underline">GitHub Repo</a>
+            </div>
           </div>
         </div>
       </Container>
