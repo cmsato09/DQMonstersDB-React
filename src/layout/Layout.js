586fb8c70016540507ed38892a76d75327dde6e0
Chris Sato <cmsato09.git@gmail.com>
2025-05-05 14:27:53 -0700

    Add id='top' to Layout for 'Back to Top' link in Footer

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div id="top" className="flex flex-col min-h-screen ">
       <Header />
       <main className="flex-grow">
         <Outlet />
