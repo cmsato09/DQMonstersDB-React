bf85cb4da4653b97315106831adc00fddec75584
Chris Sato <cmsato09.git@gmail.com>
2025-05-09 18:18:35 -0700

    Implemented responsive logo
    - title in navbar condenses to "DQM" on small screens
    - shows "DQ Monsters Database" on larger screens

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,10 +6,12 @@ function Header(){
     <header className="bg-gray-800 text-white p-4">
       <Container size="3">
           <div className="flex items-center space-x-10">
-            <div className="text-xl font-bold">
+            <div className="text-xl font-bold hidden sm:block">
               DQ Monsters Database
             </div>
-            
+            <div className="text-xl font-bold sm:hidden">
+              DQM
+            </div>
             <Navbar />
           </div>
       </Container>
