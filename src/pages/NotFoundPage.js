7bfab5780c8724d37b86126faccd91197efa6ace
Chris Sato <cmsato09.git@gmail.com>
2025-05-10 00:15:06 -0700

    Reformat NotFoundPage to display message in center of screen

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
+import { Container, Heading} from "@radix-ui/themes";
+
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
 
 function NotFoundPage() {
   return (
-    <div className="flex flex-col gap-2">
-      <h1>404 Not Found</h1>
-      <Link to="/">Go to Home Page</Link>
+    <div id="top" className="flex flex-col min-h-screen ">
+      <Header />
+      <Container size="3" classname="flex-1">
+        <main className="min-h-[calc(75vh-8rem)] flex flex-col items-center justify-center gap-4">
+          <Heading>Page Not Found</Heading>
+          <div>
+            Go Back or Go to <Link to="/" className="text-blue-500 hover:underline">Home Page</Link>
+          </div>
+          <img 
+              src="/page-not-found.png"
+              alt="404 Slime"
+              className="w-64 h-auto mt-4"
+          />
+        </main>
+      </Container>
+      <Footer />
     </div>)
 }
 
